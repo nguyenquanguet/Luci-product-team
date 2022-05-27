a7e1d04c8b4419063380d15899b6314f44317768
Nguyequanguet <nguyenquanguet@gmail.com>
2022-05-27 08:45:49 +0700

    fix file

---
+++ ApplicationCRUD/application/src/main/java/com/example/application/ServiceConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.application;
+
+import org.example.Dao;
+import org.example.dao.DaoImp;
+import org.example.dao.UserRepository;
+import org.example.service.Service;
+import org.example.service.ServiceConfig;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@ComponentScan({"org.example.dao", "org.example.controller"})
+@EnableJpaRepositories("org.example.dao")
+@EntityScan("org.example.entity")
+@Import(ServiceConfig.class)
+public class ServiceConfiguration {
+
+//    @Bean
+//    public Service service(Dao dao){
+//        return new Service(dao);
+//    }
+    @Bean
+    public Dao dao(UserRepository userRepository){
+        return new DaoImp(userRepository);
+    }
+}
